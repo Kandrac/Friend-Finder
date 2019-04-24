090ae7e1c2ba8fa9b9de1aacdb9d91f1e835d4dd
unknown <b_kand@yahoo.com>
2019-04-24 01:05:35 -0400

    Working App

---
+++ data/friends.js
@@ -0,0 +1,198 @@
+var avengersMatch = [
+
+  {
+      name: 'Thor',
+      photo: 'https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/11/03/104819285-thor.500x500.jpg?v=1509730606',
+      scores: [
+         2,
+         5,
+         4,
+         5,
+         3,
+         2,
+         2,
+         4,
+         4,
+         4,
+      ]
+  },
+
+  {
+      name: 'Tony Stark',
+      photo: 'https://img1.looper.com/img/gallery/why-we-never-got-to-see-iron-man-4/intro.jpg?auto=format%2Ccompress&h=500&w=500&crop=edges&fit=crop',
+      scores: [
+        2,
+        1,
+        1,
+        5,
+        4,
+        5,
+        5,
+        5,
+        3,
+        3,
+
+      ]
+  },
+
+  {
+      name: 'Carol Danverseo',
+      photo: 'http://gannonknight.com/wp-content/uploads/2019/03/Captain-Marvel-Poster-new-look-1-500x500.jpg',
+      scores: [
+        2,
+        3,
+        5,
+        5,
+        2,
+        1,
+        2,
+        5,
+        5,
+        1,
+      ]
+  },
+
+  {
+      name: 'Natasha Romanoff ',
+      photo: 'https://cdn.vox-cdn.com/thumbor/tusPQdkdtQ2AqegBDJMAKv9amu4=/0x0:1280x640/1200x800/filters:focal(500x500:500x500)/cdn.vox-cdn.com/uploads/chorus_image/image/60256343/black-widow.0.jpg',
+      scores: [
+          1,
+          1,
+          1,
+          1,
+          3,
+          4,
+          2,
+          3,
+          3,
+          2,
+      ]
+  },
+
+  {
+      name: "Steve Rogers",
+      photo: 'https://cdn.drawception.com/images/panels/2012/5-13/QwByzYjRt1-2.png?auto=format%2Ccompress&h=500&w=500&crop=edges&fit=crop',
+      scores: [
+          1,
+          1,
+          3,
+          1,
+          1,
+          1,
+          1,
+          1,
+          3,
+          4,
+      ]
+  },
+
+  {
+      name: 'Thanos',
+      photo: 'https://i.kinja-img.com/gawker-media/image/upload/s--7G_xxjFj--/c_fill,fl_progressive,g_center,h_500,q_80,w_500/layqlsejklmdghxevoac.jpg',
+      scores: [
+          5,
+          5,
+          2,
+          5,
+          5,
+          1,
+          3,
+          3,
+          4,
+          5,
+
+      ]
+  },
+
+  {
+      name: 'Scott Lang',
+      photo: 'https://cdn1.thr.com/sites/default/files/imagecache/landscape_500x500/2015/04/ant-man_trailer_screengrab_2_h_2015.jpg',
+      scores: [
+        1,
+        1,
+        2,
+        1,
+        2,
+        5,
+        5,
+        2,
+        5,
+        5,
+
+      ]
+  },
+
+  {
+      name: 'Loki',
+      photo: 'https://fsmedia.imgix.net/27/b5/75/d4/6f19/461a/afe5/2748b392261e/tom-hiddleston-as-loki-in-thor-ragnarok.png?auto=format%2Ccompress&h=500&w=500&crop=edges&fit=crop',
+      scores: [
+        3,
+        5,
+        4,
+        5,
+        5,
+        1,
+        5,
+        4,
+        1,
+        2,
+
+      ]
+  },
+
+
+  {
+      name: 'Peter Parker',
+      photo: 'https://assets1.ignimgs.com/2018/08/01/spiderman-peter-blogroll-1533165706834_500w.jpg',
+      scores: [
+         1,
+         1,
+         4,
+         4,
+         1,
+         5,
+         5,
+         4,
+         1,
+         5,
+
+      ]
+  },
+  {
+    name: 'Bruce Banner',
+    photo: 'https://i.ytimg.com/vi/hwaku6uNZ-o/maxresdefault.jpg?auto=format%2Ccompress&h=500&w=500&crop=edges&fit=crop',
+    scores: [
+       1,
+       1,
+       5,
+       5,
+       5,
+       2,
+       3,
+       1,
+       2,
+       2,
+
+    ]
+},
+
+  {
+      name: 'Wanda Maximoff',
+      photo: 'https://nypdecider.files.wordpress.com/2019/02/scarlet-witch-infinity-war.jpg?quality=90&strip=all&w=500&h=500&crop=1',
+      scores: [
+          1,
+          1,
+          5,
+          1,
+          3,
+          5,
+          1,
+          4,
+          1,
+          5,
+
+      ]
+  }
+]
+
+module.exports = avengersMatch;
